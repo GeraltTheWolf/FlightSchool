2f13e7f4a61a73308f118cd2e0615ca946181a54
PC-REZEK\irezek <irezek@outlook.com>
2019-12-08 14:12:51 +0100

    init project from intellj

---
+++ src/main/java/hr/rezek/flightschool/FlightschoolApplication.java
@@ -0,0 +1,13 @@
+package hr.rezek.flightschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightschoolApplication.class, args);
+    }
+
+}
